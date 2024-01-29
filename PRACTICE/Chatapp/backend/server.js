fb59f23275afe0733388ae73621aee5ec83d4da2
Joy khulbe <khulbejoy.jk@gmail.com>
2024-01-29 17:48:40 +0530

    Update server.js

--- PRACTICE/Chatapp/backend/server.js
+++ PRACTICE/Chatapp/backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const {chats}=require ("./data/data");
 
 const app = express();
 
 app.get('/',(req,res)=> {
-    res.send("APi is running")
+    res.send("APi is running");
+});
+
+app.get('/api/chat',(req, res)=> {
+    res.send(chats);
 });
 
 app.listen(5000, console.log("Server Port on 5000"));
