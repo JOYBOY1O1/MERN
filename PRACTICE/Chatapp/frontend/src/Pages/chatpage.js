3626044e7845c3cfa6addadd439d71f47cd55e52
Joy khulbe <khulbejoy.jk@gmail.com>
2024-01-31 00:37:15 +0530

    Update chatpage.js

--- PRACTICE/Chatapp/frontend/src/Pages/chatpage.js
+++ PRACTICE/Chatapp/frontend/src/Pages/chatpage.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
 
-    const [chats, setchats] = useState([]);
-    const fetchchats = async() => {
-        const {data} = await axios.get('/api/chat');
-        setchats(data);
-    }
+  const fetchchats = async () => {
+    const { data } = await axios.get("/api/chat");
 
-    useEffect(() => {
-        fetchchats();
-    },[]);
+    setChats(data);
+  };
 
-return ( <div> {chats.map()} </div> )
-}
+  useEffect(() => {
+    fetchchats();
+  }, []);
 
-export default ChatPage
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div>{chat.chatName}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ChatPage;
