eea2346b4af4c7c4fdcb15f2055acdb6b65697af
Joy khulbe <khulbejoy.jk@gmail.com>
2024-01-31 22:45:20 +0530

    Update chatModel.js

--- PRACTICE/Chatapp/backend/models/chatModel.js
+++ PRACTICE/Chatapp/backend/models/chatModel.js
@@ -1,1 +1,24 @@
-//chat name
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
+    latestMesssage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const chat = mongoose.model("Chat", chatModel);
+module.exports = Chat;
