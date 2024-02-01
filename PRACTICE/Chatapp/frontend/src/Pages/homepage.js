feec25239cd6c324c33e9d63a5cd58940656f560
Joy khulbe <khulbejoy.jk@gmail.com>
2024-02-01 23:49:15 +0530

    Update homepage.js

--- PRACTICE/Chatapp/frontend/src/Pages/homepage.js
+++ PRACTICE/Chatapp/frontend/src/Pages/homepage.js
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 const HomePage = () => {
   return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"1g"}
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" color="white">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box w="100%" p={4} borderRadius="lg" borderWidth="1px"></Box>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
