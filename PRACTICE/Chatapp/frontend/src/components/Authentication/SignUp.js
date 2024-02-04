3e8bc4e8ea01fc3cf342ccbd04c2b329ddf25a54
Joy khulbe <khulbejoy.jk@gmail.com>
2024-02-04 19:44:55 +0530

    Update SignUp.js

--- PRACTICE/Chatapp/frontend/src/components/Authentication/SignUp.js
+++ PRACTICE/Chatapp/frontend/src/components/Authentication/SignUp.js
@@ -2,18 +2,23 @@ import { FormControl, FormLabel, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const SignUp = () => {
-  const[name,setName]=useState();
-  const[email,setEmail]=useState();
-  const[confirmpassword,setConfrimpassword]=useState();
-  const[password,setPassword]=useState();
-  const[pic,setPic]=useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [confirmpassword, setConfrimpassword] = useState();
+  const [password, setPassword] = useState();
+  const [pic, setPic] = useState();
 
-  return <VStack spacing="5px">
-    <FormControl>
-      <FormLabel></FormLabel>
-      <Input placeholder='Enter your Name' onchange={()=>}></Input>
-    </FormControl>
-    </VStack>;
+  return (
+    <VStack spacing="5px" color={"white"}>
+      <FormControl id="first-name" isRequired>
+        <FormLabel></FormLabel>
+        <Input
+          placeholder="Enter your Name"
+          onchange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+    </VStack>
+  );
 };
 
 export default SignUp;
