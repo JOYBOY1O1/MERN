670ac3da1574609c246a114745736ff2f83968dd
Joy khulbe <khulbejoy.jk@gmail.com>
2024-02-08 00:47:53 +0530

    Update Login.js

--- PRACTICE/Chatapp/frontend/src/components/Authentication/Login.js
+++ PRACTICE/Chatapp/frontend/src/components/Authentication/Login.js
@@ -1,7 +1,57 @@
-import React from "react";
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => setShow(!show);
+
+  const sumbitHandler = () => {};
+
+  return (
+    <VStack spacing="5px" color={"white"}>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter your Email"
+          onchange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter your Password"
+            onchange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={sumbitHandler}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
