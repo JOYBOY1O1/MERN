43860cd39f0d036a4421597962270d5c00d97a9c
Joy khulbe <khulbejoy.jk@gmail.com>
2024-02-22 19:17:14 +0530

    Update db.js

--- PRACTICE/Chatapp/backend/config/db.js
+++ PRACTICE/Chatapp/backend/config/db.js
@@ -5,11 +5,10 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindandModify: true,
     });
     console.log(`MONGODB CONNECTED ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.bold);
     process.exit();
   }
 };
