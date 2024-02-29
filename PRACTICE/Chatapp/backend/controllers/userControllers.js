d77f6268ecd61d895ddd27b0a8393b2d92a49635
Joy khulbe <khulbejoy.jk@gmail.com>
2024-02-29 11:08:20 +0530

    Update userControllers.js

--- PRACTICE/Chatapp/backend/controllers/userControllers.js
+++ PRACTICE/Chatapp/backend/controllers/userControllers.js
@@ -2,4 +2,10 @@ const asyncHandler = require("express-aysnc-Handler");
 
 const registerUser = asyncHandler(async () => {
   const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+  const userExist = await User.findOne({ email });
+  //mongo db ki query hai to check is present or not
 });
