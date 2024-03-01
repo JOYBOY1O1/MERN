a966cfc94b412272096fa8716b808a7e885e14c1
Joy khulbe <khulbejoy.jk@gmail.com>
2024-03-01 11:17:49 +0530

    Update server.js

--- PRACTICE/Chatapp/backend/server.js
+++ PRACTICE/Chatapp/backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const { connect } = require("mongoose");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require(".routes/userRoutes");
 dotenv.config();
 connectDB();
 const app = express();
@@ -12,15 +13,7 @@ app.get("/", (req, res) => {
   res.send("API is RUNNING sucessfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  //console.log(req.params.id);
-  const singlechat = chats.find((c) => c._id === req.params.id);
-  res.send(singlechat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Running on Port: ${PORT}`.yellow.bold));
