f517bc5aa1d06ce7ee4754fdc34225f4b210287f
Joy khulbe <khulbejoy.jk@gmail.com>
2024-03-04 15:35:42 +0530

    Update 1_async.js

--- JavaScript/1_async.js
+++ JavaScript/1_async.js
@@ -1,1 +1,16 @@
+const dummy_data = [
+  { name: "Aman", Profession: "Software Engineer" },
+  { name: "Anuj", Profession: "Software Engineer" },
+];
 
+function get_data() {
+  setTimeout(() => {
+    let op = "";
+    dummy_data.forEach((data, index) => {
+      op += `<li>${data.name}</li>`;
+    });
+    document.body.innerHTML = op;
+  }, 1000);
+}
+
+get_data();
